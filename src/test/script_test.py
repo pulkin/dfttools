832820b26a771d8f24014e529e2a97702aa96f34
Artem Pulkin <gpulkin@gmail.com>
2017-05-16 17:47:03 -0700

    Added structure visualisation script and a basic test case for it

--- src/test/script_test.py
+++ src/test/script_test.py
@@ -1,25 +1,44 @@
 import unittest
 import os, sys, subprocess
 
+def get_fname(self, id):
+    return "__"+self.__class__.__name__+"TEMP"+str(id)        
+
 class Test_dft_plot_bands(unittest.TestCase):
 
-    def get_fname(self, id):
-        return "__"+self.__class__.__name__+"TEMP"+str(id)
-        
     def setUp(self):
-        fname = self.get_fname("0.pdf")
+        fname = get_fname(self,"0.pdf")
         if os.path.exists(fname):
             os.remove(fname)
             
     tearDown = setUp
         
     def test_output(self):
-        fname = self.get_fname("0.pdf")
+        fname = get_fname(self,"0.pdf")
         assert subprocess.call((
             sys.executable,
             "scripts/dft-plot-bands",
             "test/parsers/cases/qe.output.0.testcase",
             "-o",
+            fname,
+        )) == 0
+        assert os.path.exists(fname)
+
+class Test_dft_svg_struct(unittest.TestCase):
+        
+    def setUp(self):
+        fname = get_fname(self,"0.svg")
+        if os.path.exists(fname):
+            os.remove(fname)
+            
+    tearDown = setUp
+        
+    def test_output(self):
+        fname = get_fname(self,"0.svg")
+        assert subprocess.call((
+            sys.executable,
+            "scripts/dft-svg-structure",
+            "test/parsers/cases/qe.output.0.testcase",
             fname,
         )) == 0
         assert os.path.exists(fname)
